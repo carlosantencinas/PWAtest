04386519b33d2d6db33381c346937c451faf1ae7
carlosantencinas <64339850+carlosantencinas@users.noreply.github.com>
2025-05-27 17:09:40 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,12 +2,22 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('calc-cache').then(cache => {
       return cache.addAll([
-        '/',
         'index.html',
-        'style.css',
+        'styles.css',   // cuidado con el nombre, debe ser exacto
         'app.js',
         'manifest.json'
       ]);
+    })
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== 'calc-cache')
+            .map(key => caches.delete(key))
+      );
     })
   );
 });
